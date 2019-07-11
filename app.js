41301bdd1381d98709bc21727fc40b6de574c8ed
Igor Silva <igor.silva@wdev.int>
2019-07-11 17:34:11 -0300

    Adicionando a porta no log

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ const PORT = process.env.PORT || 3000;
 
 /* parametrizar a porta de escuta */
 app.listen(PORT, function(){
-	console.log('Servidor online');
+	console.log('Servidor online. Rodando na porta: ' + PORT);
 })
